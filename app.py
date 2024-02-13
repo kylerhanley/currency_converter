9dccbb855dda9e523f39249653773111a26acd67
Kyle Hanley <kylerhanley1997@gmail.com>
2024-02-12 17:05:27 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect
+import requests
+
+app = Flask(__name__)
+@app.route("/", methods=["GET","POST"])
+def home():
+    if request.method == "GET":
+        return render_template("home.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
